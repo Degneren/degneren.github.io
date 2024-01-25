6269f633ea352db9a4b52b5f69a6d9bf11c9d2e9
Degneren <ChristianDegn@gmail.com>
2024-01-25 11:47:28 +0100

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -1,3 +1,18 @@
+function SetCookie(Name, Value, Expiration)
+{
+  // Create a date variable that contains
+  // the expiration date.
+  var ExpDate = new Date();
+  if (Expiration != null)
+   ExpDate.setDate(ExpDate.getDate() +
+           Expiration);
+  // Encode the data for storage.
+  var CookieValue = escape(Value) +
+   "; expires=" + ExpDate.toUTCString();
+  // Store the cookie.
+  document.cookie = Name + "=" + CookieValue;
+}
+
 document.addEventListener("DOMContentLoaded", function(){
 SetCookie('externalScriptCookie', 'externalScriptCookieValue', 7)
 });
