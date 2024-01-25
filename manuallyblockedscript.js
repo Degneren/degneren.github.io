8b1ca1edef9d877ee198a3b903af8208f29faed4
Degneren <ChristianDegn@gmail.com>
2024-01-25 15:20:30 +0100

    Update manuallyblockedscript.js

--- manuallyblockedscript.js
+++ manuallyblockedscript.js
@@ -1,1 +1,17 @@
+function SetCookie(Name, Value, Expiration)
+{
+  // Create a date variable that contains
+  // the expiration date.
+  var ExpDate = new Date();
+  if (Expiration != null)
+   ExpDate.setDate(ExpDate.getDate() +
+           Expiration);
+  // Encode the data for storage.
+  var CookieValue = escape(Value) +
+   "; expires=" + ExpDate.toUTCString();
+  // Store the cookie.
+  document.cookie = Name + "=" + CookieValue;
+}
+
+
 SetCookie('manuallyBlockedCookie', 'manuallyBlockedCookieValue', 7)
